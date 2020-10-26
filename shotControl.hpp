f060a3aa2523674c7d03b39f559a6776bd33136e
Koen <koen@bravemail.nl>
2020-10-26 20:58:53 +0100

    Add shotControl.hpp with body

---
+++ shotControl.hpp
@@ -0,0 +1,53 @@
+#include "hwlib.hpp"
+#include "rtos.hpp"
+
+class sendControl : public rtos::task<>{
+    enum state_s       = {INACTIVE, SENDING};
+
+private:
+    state_s state = INACTIVE;
+
+    rtos::channel messageChannel;
+
+    IrLed irled;
+    RedLed redLed;
+public:
+    sendControl(IrLed irLed, RedLed, redLed);
+
+    void sendMesasge(uint_fast16_t message){
+
+    }
+
+};
+
+
+
+int main( void ){	
+   
+   // IR output LED
+   auto ir = hwlib::target::d2_36kHz();
+   
+   // red output LED
+   auto red = hwlib::target::pin_out( hwlib::target::pins::d7);
+   
+   // switch which enables the 36 kHz output
+   auto sw = hwlib::target::pin_in( hwlib::target::pins::d43 );
+   
+   // when the switch is pressed, 
+   // repeat sending 1 ms signal and 1 ms silence
+   // and show this on the LED
+   for(;;){
+      hwlib::wait_ms( 1 ); 
+      
+      sw.refresh();
+      ir.write( ! sw.read() );
+      red.write( ! sw.read() );
+      ir.flush();
+      red.flush();
+      
+      hwlib::wait_ms( 1 );
+      ir.write( 0 );
+      ir.flush();
+   }
+}
+
