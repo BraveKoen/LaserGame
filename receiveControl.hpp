4d7eb0c3a3f861d6151c92c75d7386f8e8562dce
Rick Horeman <rickhoreman@gmail.com>
2020-10-28 17:23:28 +0100

    ReceiveControl.hpp V0.3, veel beter en stabieler met hulp van jan

--- receiveControl.hpp
+++ receiveControl.hpp
@@ -11,7 +11,7 @@ class ReceiveControl : public rtos::task<>{
 private:
     states state = WAITING;
 
-    rtos::clock clock_400us;
+    rtos::clock clock_200us;
     rtos::timer timer_4ms;
 
     IrReceiver irReceiver;
@@ -20,7 +20,7 @@ private:
 public:
     ReceiveControl():
     task("ReceiveTask"),
-    clock_400us(this, 400, "400 us clock"), //sneller
+    clock_200us(this, 200, "200 us clock"), 
     timer_4ms(this, "4 ms timer"),
     irReceiver(hwlib::target::pins::d8)//,
     //decoder()
@@ -32,8 +32,7 @@ private:
         uint_fast8_t messageSize=0;
         uint_fast16_t message=0;
         uint_fast8_t signalHigh=0;
-        uint_fast8_t signalLow=0;
-        int debug=0;
+        //int debug=0;
 
         for(;;){
             //hwlib::cout<<"ReceiveControl main loop\n";
@@ -41,62 +40,54 @@ private:
             {
                 case WAITING:
                     //hwlib::cout<<"ReceiveControl WAITNG\n";
-                    wait(clock_400us);
+                    wait(clock_200us);
                     signal = irReceiver.read();
                     //hwlib::cout<<signal<<" signal\n";
                     if(signal){
                         messageSize=0;
                         message=0;
                         signalHigh=1;
-                        signalLow=0;
                         timer_4ms.clear();
                         timer_4ms.set(4'000);
-                        debug=1;
+                        //debug=1;
                         state = RECEIVINGMESSAGE;
                     }
                     break;
                 case RECEIVINGMESSAGE:
                     //hwlib::cout<<"ReceiveControl RECEIVINGMESSAGE\n";
-                    auto event = wait(clock_400us + timer_4ms);
-                    if(event == clock_400us){
+                    auto event = wait(clock_200us + timer_4ms);
+                    if(event == clock_200us){
                         signal = irReceiver.read();
-                        if(signal && signalHigh<4){
+                        if(signal){
                             signalHigh++;
-                            debug=debug<<1;
-                            debug++;
-                        }else if(!signal && signalLow<4){ //signal high tellen, signal low niet meer nodig
-                            signalLow++;
-                            debug=debug<<1;
-                        }else{
-                            hwlib::cout<<"high: "<<signalHigh<<" low: "<<signalLow<<" debug: "<<hwlib::bin<<debug<<"\n";
-                            signalHigh=0;
-                            signalLow=0;
-                            break;
+                            //debug=debug<<1;
+                            //debug++;
+                        }else if(signalHigh>0){
+                            if(signalHigh<6){
+                                message=message<<1;
+                                messageSize++;
+                                //hwlib::cout<<"message: "<<message<<" message size: "<<messageSize<<"\n";
+                            }else{
+                                message=message<<1;
+                                message++;
+                                messageSize++;
+                            }
+                            //debug=debug<<1;
+                            if(messageSize<=16){
+                                //hwlib::cout<<"YEET";
+                                timer_4ms.set(4'000);
+                                signalHigh=0;
+                                //debug=0;
+                            }else{
+                                //decoder.decode(message);
+                                hwlib::cout<<"message: ";
+                                hwlib::cout <<hwlib::dec<< message << "\n";
+                                state = WAITING;
+                            }
                         }
                         //hwlib::cout<<"high: "<<signalHigh<<" low: "<<signalLow<<" debug: "<<debug<<"\n";
-                        if(signalHigh==2 && signalLow==4){ //klopt niet altijd
-                            message=message<<1;
-                            messageSize++;
-                            //hwlib::cout<<"message: "<<message<<" message size: "<<messageSize<<"\n";
-                        }else if(signalHigh==4 && signalLow==2){ //klopt niet altijd
-                            message=message<<1;
-                            message++;
-                            messageSize++;
-                        }else{
-                            break;
-                        }
-                        if(messageSize<16){
-                            //hwlib::cout<<"YEET";
-                            timer_4ms.set(4'000);
-                            signalHigh=0;
-                            signalLow=0;
-                            debug=0;
-                        }else{
-                            //decoder.decode(message);
-                            hwlib::cout<<"message: ";
-                            hwlib::cout <<hwlib::dec<< message << "\n";
-                            state = WAITING;
-                        }
+                        
+                        
                     }else if(event == timer_4ms){
                         hwlib::cout<<"TIMEOUT, message length: "<<hwlib::dec<<messageSize<<"\n";
                         state = WAITING;
