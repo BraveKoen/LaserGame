4054b46b8f0d90bbb1ad87b117257f9078c70bed
joeri-hu <joeri.j.kok@student.hu.nl>
2020-10-29 13:50:40 +0100

    minor fixes + note update of write msg

--- src/boundary/display.hpp
+++ src/boundary/display.hpp
@@ -3,7 +3,6 @@
 
 #include "hwlib.hpp"
 #include "rtos.hpp"
-#include "boundary/display.hpp"
 
 class Display: public rtos::task<> {
 public:
@@ -11,6 +10,7 @@ public:
         hwlib::target::pins scl_pin,
         hwlib::target::pins sda_pin
     ):
+        task("display task"),
         scl(scl_pin),
         sda(sda_pin),
         i2c_bus(scl, sda),
@@ -24,7 +24,7 @@ public:
 private:
     hwlib::target::pin_oc scl;
     hwlib::target::pin_oc sda;
-    hwlib::12c_bus_bit_banged_scl_sda i2c_bus;
+    hwlib::i2c_bus_bit_banged_scl_sda i2c_bus;
     hwlib::glcd_oled display;
     hwlib::font_default_8x8 font;
     hwlib::terminal_from terminal;
@@ -60,25 +60,13 @@ private:
     }
 
     void writingMessage() {
+        // note: currently, there are no wait calls active at
+        // glcd_i2c's flush implementation
+        //
+        // to-do: edit flush() function of glcd_oled_i2c_128x64_buffered class
+        // file: hwlib-glcd-oled.hpp
+        // line: 459 
         terminal << message;
-        // mag er ge-switcht worden naar een andere state
-        // zonder dat een event daar verantwoordelijk voor is?
-        //
-        // in de RTOS reader staat namelijk:
-        // (de Design Like a Robot reader hint hier ook naar)
-        //
-        // "A classic way to translate a state diagram to a task is to make
-        // an enumerate for the states, and have a big switch statement in
-        // the for(;;) loop that selects on this enumerate. In each such case,
-        // you code:
-        //   • the entry actions of the state,
-        //   • wait for the event that leaves the state,  <-----
-        //   • perform the actions that are associated with leaving the state
-        //   • and with the state change,
-        //   • and finally set the new state.
-        // Don’t forget the final break stement!"
-        //
-        // misschien even navragen bij Marius?
         state = State::Inactive;
     }
 public:
@@ -96,7 +84,7 @@ public:
     }
 
     void displayMessage(messageType message) {
-        // literal strings have static storage duration so
+        // string literals have static storage duration so
         // writing the addresses of it to a pool seems fine
         messagePool.write(message);
         messageFlag.set();
