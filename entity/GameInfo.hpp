9fab0e763e075a3997aa6507e13c2d9b2c7d5e73
BraveKoen <koen@bravemail.nl>
2020-10-29 16:02:00 +0100

    added getters and setters

---
+++ entity/GameInfo.hpp
@@ -0,0 +1,33 @@
+class GameInfo{
+private:
+    unsigned int PlayerID;
+    unsigned int gameTime;
+    unsigned int weaponType;
+    int hits[100][2];
+
+public:
+   GameInfo():{}
+
+   void setWeapon(int type){
+       weaponType = type;
+   }
+   int getWeapon(){
+       return weaponType;
+    }    
+   
+    void setPlayerID(int id){
+       PlayerID = id;
+    }
+
+    int getPlayerID(){
+        return PlayerID;
+    }
+
+   void setTime(int time){
+       gameTime = time;
+    }
+   int getTime(){
+       return gameTime;
+    }
+
+};
