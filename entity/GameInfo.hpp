aab2be86d7f80ad78a9ac3f16d83827d2bbf2dd6
BraveKoen <koen@bravemail.nl>
2020-10-29 16:47:41 +0100

    added pools

--- entity/GameInfo.hpp
+++ entity/GameInfo.hpp
@@ -1,33 +1,35 @@
-class GameInfo{
+#include "hwlib.hpp"
+#include "rtos.hpp"
+
+class GameInfo : public rtos::task<> {
 private:
+    rtos::pool<int> playerID;
+    rtos::pool<int> gameTime;
+    rtos::pool<int> weaponType;
+
     unsigned int PlayerID;
     unsigned int gameTime;
     unsigned int weaponType;
     int hits[100][2];
 
 public:
-   GameInfo():{}
+    GameInfo():
+        task("GameInfo"),
+        playerID("playerIDPool"),
+        gameTime("gameTimePool"),
+        weaponType("weaponTypePool")
 
-   void setWeapon(int type){
-       weaponType = type;
-   }
-   int getWeapon(){
-       return weaponType;
-    }    
-   
-    void setPlayerID(int id){
-       PlayerID = id;
-    }//stupid
+    {}
 
-    int getPlayerID(){
-        return PlayerID;
-    }
+    void setWeapon(int type) { weaponType.write(type); }
+    
+    int getWeapon() { return weaponType.read(); }
 
-   void setTime(int time){
-       gameTime = time;
-    }
-   int getTime(){
-       return gameTime;
-    }
+    void setPlayerID(int id) { playerID.write(id); }
 
+    int getPlayerID() { return PlayerID.read(); }
+
+    void setTime(int time) { gameTime.write(time); }
+    
+    int getTime() { return gameTime.read(); }
 };
