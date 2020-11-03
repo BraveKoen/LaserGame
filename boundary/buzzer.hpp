5212b9c28a0033e357ab9d1c40376b20f0b982a0
Koen <koen@bravemail.nl>
2020-11-03 16:12:25 +0100

    bzz

--- boundary/buzzer.hpp
+++ boundary/buzzer.hpp
@@ -28,6 +28,30 @@ public:
         playSoundPool.write(on);
         playSoundFlag.set();
     }
+
+
+    void await( long long unsigned int t ){
+        while( t > hwlib::now_us() ){}
+    }
+
+    void beep(int f, int d, int fd = 1000 ){
+        auto t = hwlib::now_us();    
+        auto end = t + d;
+        while( end > hwlib::now_us() ){
+            auto p = 500'000 / f;
+            f = f * fd / 1000;
+            buzzerPin.write( 1 );
+            buzzerPin.flush();
+            await( t += p );
+            buzzerPin.write( 0 );
+            buzzerPin.flush();
+            await( t += p );
+        }   
+    }
+
+    void peew(){
+        beep(20'000, 200'000, 990 );
+    }
     
 private:
     void main(){
