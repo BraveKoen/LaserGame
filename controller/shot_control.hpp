daf0050f457071db9dd0eb5e8887f5559fd1a75e
JorisHeemskerk <60688818+JorisHeemskerk@users.noreply.github.com>
2020-11-03 09:35:51 +0100

    some doxygen added

--- controller/shot_control.hpp
+++ controller/shot_control.hpp
@@ -9,7 +9,10 @@
 #include "../boundary/trigger.hpp"
 #include "../boundary/keypad.hpp"
 
-
+/// \brief
+/// Class ShotControl, class makes players able to shoot at others. 
+/// \details
+/// When at play this class will wait for the correct buttons to be pressed and then it will shoot shot data.
 class ShotControl : public rtos::task<>, public ButtonListener{
 
 enum state_t {INACTIVE, ACTIVE};
@@ -34,6 +37,12 @@ private:
 
     
 public:
+    /// \brief
+    /// This constructor makes new instance of ShotControl, gives it a static task. 
+    /// \details
+    /// The constructor requests a SendControl, GameInfo and Keypad.
+    /// It also initializes a pool and three flags, each with fixed tasks.
+    /// A ButtonListener will be added to both the keypad and the trigger.
     ShotControl(SendControl& sendControl, GameInfo& gameInfo, Keypad& keypad):
     task( "ShotTask" ),
     sendControl(sendControl),
@@ -50,19 +59,43 @@ public:
         trigger.addButtonListener(this);
         //Unsure if multiple classes should add the same keypad <- listener should make sure it doesnt have dupes, dont know if it does
     }
-
+    /// \brief
+    /// Public function buttonPressed requests an int and puts it in a pool 
+    /// \details
+    /// When called, this function sets the buttonFlag and puts the buttonID into the buttonPool
     void buttonPressed(int buttonID){
         buttonFlag.set();
         buttonPool.write(buttonID);
     }
+    /// \brief
+    /// Public function starts sets the startFlag 
+    /// \details
+    /// Nothing special, this function sets the sart flag, making other classes able to start up this one.
     void start(){
         startFlag.set();
     }
+    /// \brief
+    /// Public function gameOver sets the gameOverFlag. 
+    /// \details
+    /// Simple function that lets other classes set the gameOverFlag.
     void gameOver(){
         gameOverFlag.set();
     }
 
 private:
+    /// \brief
+    /// Private function main makes the players able to shoot.
+    /// \details
+    /// case INACTIVE
+    ///     This case waits for the startFlag,
+    ///     saves the weaponType and playerID
+    ///     and then sets the state to ACTIVE.
+    /// case ACTIVE
+    ///     This case waits for either a button imput or for a gameOverFlag.
+    ///     If the gameOverFlag is called the state will be put back to INACTIVE.
+    ///     If a button is pressed it will be put into the buttonPool.
+    ///     The pool will be read, if its the correct buttonID,
+    ///     the sendControl will be sent the shot message needed.
     void main(){
         unsigned int weapon;
         unsigned int playerID;
@@ -91,7 +124,6 @@ private:
             }
         }
     }
-    //verrandering
 };
 
 #endif // SHOT_CONTROL_HPP
--- controller/transfer_control.hpp
+++ controller/transfer_control.hpp
@@ -6,6 +6,10 @@
 #include "../entity/game_info.hpp"
 #include <array>
 
+    /// \brief
+    /// This class TransferControl puts the hits received into the pc terminal. 
+    /// \details
+    /// When certain flags are set this function will hwlib::cout the hits from ../entity/game_info.
 class TransferControl : public rtos::task<>{
 enum state_t { INACTIVE, ACTIVE };
 private:
@@ -13,11 +17,21 @@ private:
     rtos::flag transferFlag;
     state_t state = INACTIVE;
 public:
+    /// \brief
+    /// Contructor for class TransferControl
+    /// \details
+    /// This constructor gives TransferControl a default task since only 1 instance is needed. 
+    /// It receives the gameinfo by reference.
+    /// It also has a flag called transferFlag.
     TransferControl(GameInfo& gameInfo):
     task("TransferControl"),
     gameInfo(gameInfo),
     transferFlag(this, "transferFlag"){}
 
+    /// \brief
+    /// Public Function transferCommand. Sets Flag.
+    /// \details
+    /// This function simply sets the transferFlag when called.
     void transferCommand(){
         transferFlag.set();
     }
@@ -27,10 +41,21 @@ private:
         for(;;){
 
             switch(state){
+                /// \brief
+                /// The inactive state waits for the transferFlag and moves to active.
+                /// \details
+                /// When this case is at play, it waits until the transferflag is set.
+                /// Then it puts the state to active and breaks so it can go back to the switch and move to the active state.
                 case INACTIVE:
                     wait(transferFlag);
                     state = ACTIVE;
                     break;
+                /// \brief
+                /// The active state gets the hits and hwlib::cout's them into the terminal.
+                /// \details
+                /// This case pulls the hits from the gameInfo and hwlib::cout's them into the terminal of the connected PC,
+                /// (wiht an open terminal able to read).
+                /// After completing its task it will put the state back to inactive.
                 case ACTIVE:
                     auto array_hits = gameInfo.getHits();
                     hwlib::cout << "Player data: "<< gameInfo.getPlayerID() << "\n";
