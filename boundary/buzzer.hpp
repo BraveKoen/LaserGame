724b076525711c8a03f7dea1f83f513d8ba70b39
Koen <koen@bravemail.nl>
2020-11-04 13:38:47 +0100

    buzzer go brrr

--- boundary/buzzer.hpp
+++ boundary/buzzer.hpp
@@ -4,33 +4,19 @@
 #include "rtos.hpp"
 #include "hwlib.hpp"
 
-class Buzzer : public rtos::task<>{
-
-enum state_t {INACTIVE, ACTIVE};
+class Buzzer{
 
 private:
     hwlib::target::pin_out buzzerPin;
-    rtos::clock clock_1500us;
-    rtos::pool<bool> playSoundPool;
-    rtos::flag playSoundFlag;
 
     state_t state = INACTIVE;
 public:
     Buzzer(hwlib::target::pins pin):
-        task("Buzzer"),
-        buzzerPin(hwlib::target::pin_out(pin)),
-        clock_1500us(this, 1500, "1500 us clock"),
-        playSoundPool("playSoundPool"),
-        playSoundFlag(this, "paySoundFlag")
+        buzzerPin(hwlib::target::pin_out(pin))
     {}
 
-    void playSound(bool on){
-        playSoundPool.write(on);
-        playSoundFlag.set();
-    }
 
-
-    void await( long long unsigned int t ){
+void await( long long unsigned int t ){
         while( t > hwlib::now_us() ){}
     }
 
@@ -51,45 +37,6 @@ public:
 
     void peew(){
         beep(20'000, 200'000, 990 );
-    }
-    
-private:
-    void main(){
-        bool poolState;
-        bool buzzerState;
-        for(;;){
-            switch(state){
-                case INACTIVE:
-                    wait(playSoundFlag);
-                    poolState = playSoundPool.read();
-                    if(poolState){
-                        buzzerState = true;
-                        state = ACTIVE;
-                        break;
-                    }else{
-                        break;
-                    }
-                case ACTIVE:
-                    auto event = wait(clock_1500us + playSoundFlag);
-                    if(event == clock_1500us){
-                        if(buzzerState == true){
-                            buzzerState = false;
-                            buzzerPin.write(true);
-                        }else{
-                            buzzerState = true;
-                            buzzerPin.write(false);
-                        }
-                    }else if(event == playSoundFlag){
-                        poolState = playSoundPool.read();
-                        if(!poolState){
-                            buzzerPin.write(false);
-                            state = INACTIVE;
-                        }
-                        break;
-                    }
-                    break;
-            }
-        }
     }
 };
 
