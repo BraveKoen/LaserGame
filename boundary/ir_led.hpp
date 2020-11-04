3c0ce7c7b15818ea30ba3a4c7a5bace4498f5f90
Koen <koen@bravemail.nl>
2020-11-04 10:10:48 +0100

    doxy boundary

--- boundary/ir_led.hpp
+++ boundary/ir_led.hpp
@@ -1,13 +1,20 @@
 #ifndef IR_LED_HPP
 #define IR_LED_HPP
 
+/// \brief
+/// Class IrLed
+/// \details
+/// IrLed that goes brrr
 class IrLed {
  private:
   hwlib::target::d2_36kHz irLed;
 
  public:
   IrLed() : irLed(hwlib::target::d2_36kHz()) {}
-
+/// \brief
+/// Function write int state: void
+/// \details
+/// Writes state to the irled and flush it
   void write(int state) {
     irLed.write(state);
     irLed.flush();
